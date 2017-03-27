65538ad262d053483b9284de172e0840fc708f5d
Iryna <irynakanivets@gmail.com>
2017-03-27 15:13:55 +0300

    Added extra screens

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {createStore, applyMiddleware} from 'redux';
 import firebase from 'firebase';
 import ReduxTHunk from 'redux-thunk';
 import reducers from './reducers';
-import LoginForm from './components/LoginForm';
+import Router from './Router';
 
 
 class App extends Component {
@@ -28,7 +28,7 @@ class App extends Component {
 
     return(
     <Provider store={store}>
-        <LoginForm/>
+        <Router/>
     </Provider>
     );
     }
---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,61 @@
+import React, {Component} from 'react';
+import {Picker} from 'react-native';
+import {connect} from 'react-redux';
+import {employeeUpdate} from '../actions';
+import {Card, CardSection, Input, Button} from './common';
+
+
+class EmployeeCreate extends Component {
+    render(){
+        return(
+            <Card>
+                <CardSection>
+                    <Input
+                       label='Name'
+                        placeholder='Jane'
+                        value={this.props.name}
+                        onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
+
+                    />
+                </CardSection>
+                <CardSection>
+                     <Input
+                         label='Phone'
+                         placeholder='555-555-555'
+                         value={this.props.phone}
+                         onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
+                    />
+                </CardSection>
+                <CardSection>
+                    <Picker
+                        style = {{ flex: 1}}
+                        selectedItem= {this.props.shift}
+                        onValueChange = {value => this.props.employeeUpdate({ prop:'shift', value  })}
+                        >
+                            <Picker.Item label='Monday' value='Monday' />
+                            <Picker.Item label='Tuesday' value='Tuesday' />
+                            <Picker.Item label='Wednesday' value='Wednesday' />
+                            <Picker.Item label='Thursday' value='Thursday' />
+                            <Picker.Item label='Friday' value='Friday' />
+                            <Picker.Item label='Saturday' value='Saturday' />
+                            <Picker.Item label='Sunday' value='Sunday' />
+
+                    </Picker>
+                </CardSection>
+                <CardSection>
+                    <Button>
+                        Save
+                    </Button>
+                </CardSection>
+            </Card>
+        );
+    }
+}
+
+const mapToStateProps = (state) => {
+    const { name, phone, shift} = state.employeeForm;
+    return {name, phone, shift};
+};
+
+
+export default connect(mapToStateProps, {employeeUpdate}) (EmployeeCreate);
