9bc3c75be6e446144cd494eb8b2c942d67011b3f
Iryna <irynakanivets@gmail.com>
2017-04-03 20:25:54 +0300

    Added creation and deleting of employees

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import {Picker, Text} from 'react-native';
 import {connect} from 'react-redux';
 import {employeeUpdate, employeeCreate} from '../actions';
-import {Card, CardSection, Input, Button} from './common';
+import {Card, CardSection, Button} from './common';
+import EmployeeForm from './EmployeeForm';
 
 
 class EmployeeCreate extends Component {
@@ -13,42 +13,13 @@ class EmployeeCreate extends Component {
     }
 
     render(){
+
+
+
+
         return(
             <Card>
-                <CardSection>
-                    <Input
-                       label='Name'
-                        placeholder='Jane'
-                        value={this.props.name}
-                        onChangeText={value => this.props.employeeUpdate({ prop: 'name', value })}
-
-                    />
-                </CardSection>
-                <CardSection>
-                     <Input
-                         label='Phone'
-                         placeholder='555-555-555'
-                         value={this.props.phone}
-                         onChangeText={value => this.props.employeeUpdate({ prop: 'phone', value })}
-                    />
-                </CardSection>
-
-                <CardSection>
-                <Text style={styles.pickerTextStyle}>Shift</Text>
-                    <Picker
-                        style = {{ flex: 1}}
-                        selectedItem= {this.props.shift}
-                        onValueChange = {value => this.props.employeeUpdate({ prop:'shift', value  })}
-                        >
-                            <Picker.Item label='Monday' value='Monday' />
-                            <Picker.Item label='Tuesday' value='Tuesday' />
-                            <Picker.Item label='Wednesday' value='Wednesday' />
-                            <Picker.Item label='Thursday' value='Thursday' />
-                            <Picker.Item label='Friday' value='Friday' />
-                            <Picker.Item label='Saturday' value='Saturday' />
-                            <Picker.Item label='Sunday' value='Sunday' />
-                    </Picker>
-                </CardSection>
+                <EmployeeForm  {...this.props} />
 
                 <CardSection>
                     <Button onPress={this.onButtonPress.bind(this)}>
@@ -60,13 +31,6 @@ class EmployeeCreate extends Component {
     }
 }
 
-const styles ={
-    pickerTextStyle: {
-        fontSize:18,
-        paddingLeft: 20
-    }
-
-}
 
 const mapToStateProps = (state) => {
     const { name, phone, shift} = state.employeeForm;
---
+++ src/reducers/EmployeeReducer.js
@@ -0,0 +1,18 @@
+import {
+    EMPLOYEES_FETCH_SUCCESS,
+
+} from '../actions/types';
+
+
+const INITIAL_STATE = { };
+
+export default (state = INITIAL_STATE, action)  => {
+     switch (action.type) {
+        case EMPLOYEES_FETCH_SUCCESS:
+            return action.payload;
+            return state;
+
+        default:
+            return state;
+     }
+};
